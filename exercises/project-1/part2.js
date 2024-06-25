78358ac6ea043a753b34b583e0a9db515a97860b
Divine Touch <divine.touch07@gmail.com>
2024-06-24 21:12:37 -0500

    using webcam

--- exercises/project-1/part2.js
+++ exercises/project-1/part2.js
@@ -1,2 +1,15 @@
 import "@tensorflow/tfjs";
-import * as ccoSsd from "@tensorflow-models/coco-ssd";
+import * as cocoSsd from "@tensorflow-models/coco-ssd";
+import { startWebcam } from "./utils";
+
+const webcamButton = document.getElementById("webcam");
+const captureButton = document.getElementById("pause");
+const video = document.querySelector("video");
+
+let model;
+
+// const init = async () => {
+//   model = await cocoSsd.load();
+// };
+
+webcamButton.onclick = () => startWebcam(video);
